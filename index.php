e5312a8718a518b50b67cff2a813beb552cf402d
Cyriaque Skrapits <cyriaque.skrapits@swisscom.com>
2023-12-07 14:14:48 +0100

    Fix code style

--- index.php
+++ index.php
@@ -2,50 +2,62 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<title>My photos</title>
 	<meta charset="utf-8">
 	<meta name="generator" content="https://github.com/cisoun/Galerie">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>My photos</title>
 	<style type="text/css">
-		:root          { --gap:               3vw; }
-		body           { background-color:    #fff;
-			             color:               #000;
-			             margin:              0;
-			             user-select:         none; }
-		body.noscroll  { overflow:            hidden; }
-		#grid          { column-count:        1;
-			             column-gap:          var(--gap);
-			             margin:              auto;
-			             padding:             calc(2 * var(--gap)); }
-		#grid img      { cursor:              zoom-in;
-			             height:              auto;
-			             margin-bottom:       var(--gap);
-			             width:               100%; }
-		#modal         { backdrop-filter:     blur(1rem);
-			             background-color:    #fffe;
-			             background-position: center;
-			             background-repeat:   no-repeat;
-			             background-size:     contain;
-			             cursor:              zoom-out;
-			             display:             none;
-			             height:              100%;
-			             justify-content:     space-between;
-			             position:            fixed;
-			             width:               100%; }
-		#modal.visible { display: flex; }
-		#modal a       { cursor:              pointer;
-			             font-size:           3rem;
-			             line-height:         100vh;
-			             text-align:          center;
-			             width:               20%; }
-		@media (prefers-color-scheme: dark) {
-			body       { background-color:    #000;
-				         color:               #fff; }
-			#modal     { background-color:    #000e; }
+		:root { --gap: 3vw; }
+		body {
+			background-color: #fff;
+			color: #000;
+			margin: 0;
+			user-select: none;
 		}
-		@media only screen and (max-width: 600px) { #modal a { display:      none; } }
-		@media only screen and (min-width: 600px) { #grid    { column-count: 2; } }
-		@media only screen and (min-width: 992px) { #grid    { column-count: 3; } }
+		body.noscroll { overflow: hidden; }
+		#grid {
+			column-count: 1;
+			column-gap: var(--gap);
+			margin: auto;
+			padding: calc(2 * var(--gap));
+		}
+		#grid img {
+			cursor: zoom-in;
+			height: auto;
+			margin-bottom: var(--gap);
+			width: 100%;
+		}
+		#modal {
+			backdrop-filter: blur(1rem);
+			background-color: #fffe;
+			background-position: center;
+			background-repeat: no-repeat;
+			background-size: contain;
+			cursor: zoom-out;
+			display: none;
+			height: 100%;
+			justify-content: space-between;
+			position: fixed;
+			width: 100%;
+		}
+		#modal.visible { display: flex; }
+		#modal a {
+			cursor: pointer;
+			font-size: 3rem;
+			line-height: 100vh;
+			text-align: center;
+			width: 20%;
+		}
+		@media (prefers-color-scheme: dark) {
+			body {
+				background-color: #000;
+				color: #fff;
+			}
+			#modal { background-color: #000e; }
+		}
+		@media only screen and (max-width: 600px) { #modal a { display: none; } }
+		@media only screen and (min-width: 600px) { #grid { column-count: 2; } }
+		@media only screen and (min-width: 992px) { #grid { column-count: 3; } }
 	</style>
 </head>
 <body>
@@ -74,8 +86,8 @@ modal.childNodes[1].onclick = e => { e.stopPropagation(); next(); }
 modal.onclick               = e => toggle(e);
 modal.ontouchstart          = e => touchX = e.changedTouches[0].clientX;
 modal.ontouchend            = e => {
-    if      (e.changedTouches[0].clientX - touchX > +50) previous();
-    else if (e.changedTouches[0].clientX - touchX < -50) next();
+	if      (e.changedTouches[0].clientX - touchX > +50) previous();
+	else if (e.changedTouches[0].clientX - touchX < -50) next();
 }
 </script>
 </body>
